384672736ac8365f49e14dfd878e24ce4ef6773e
Ivan Zubarev <swim_zb@mail.ru>
2025-05-06 21:21:43 +0700

    test

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -8,12 +8,12 @@ public class DifferTest {
     @Test
     public void testCorrectionEquals() throws Exception {
         String expected = "{\n  - proxy: 123.234.53.22"
-                            + "\n    host: hexlet.io"
-                            +  "\n  - follow: false"
-                            +  "\n  - timeout: 50"
-                            +  "\n  + timeout: 20"
-                            +  "\n  + verbose: true"
-                            +  "\n}";
+                        + "\n    host: hexlet.io"
+                        +  "\n  - follow: false"
+                        +  "\n  - timeout: 50"
+                        +  "\n  + timeout: 20"
+                        +  "\n  + verbose: true "
+                        +  "\n}";
 
         Map<String, Object> data1 = JsonParser.parseJson("file1.json");
         Map<String, Object> data2 = JsonParser.parseJson("file2.json");
