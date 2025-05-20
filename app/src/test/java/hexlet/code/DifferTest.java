3158c9100fb77057481564dd08679c4389683798
Ivan Zubarev <swim_zb@mail.ru>
2025-05-20 21:49:20 +0700

    add test for nested yaml differ

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -6,11 +6,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DifferTest {
 
     @Test
-    public void testCorrectionEquals() throws Exception {
-        String expected = JsonParser.readFixture("expected_json_output.txt");
+    public void testJson() throws Exception {
+        String expected = Parser.readFixture("expected_flat_output.txt");
 
-        Map<String, Object> data1 = JsonParser.parseJson("file1.json");
-        Map<String, Object> data2 = JsonParser.parseJson("file2.json");
+        Map<String, Object> data1 = Parser.parseJson("file1.json");
+        Map<String, Object> data2 = Parser.parseJson("file2.json");
+        String actual = Differ.generate(data1, data2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testYaml() throws Exception {
+        String expected = Parser.readFixture("expected_flat_output.txt");
+
+        Map<String, Object> data1 = Parser.parseYaml("file1.yml");
+        Map<String, Object> data2 = Parser.parseYaml("file2.yml");
+        String actual = Differ.generate(data1, data2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNestedYaml() throws Exception {
+        String expected = Parser.readFixture("expected_nested_output.txt");
+
+        Map<String, Object> data1 = Parser.parseYaml("nested1.yml");
+        Map<String, Object> data2 = Parser.parseYaml("nested2.yml");
         String actual = Differ.generate(data1, data2);
 
         assertEquals(expected, actual);
