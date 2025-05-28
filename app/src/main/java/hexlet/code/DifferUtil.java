b5d745d5585273b7bc47195b81e73d44211f5af7
Ivan Zubarev <swim_zb@mail.ru>
2025-05-28 21:30:48 +0700

    add DifferUtil for processing of Differ

--- app/src/main/java/hexlet/code/DifferUtil.java
+++ app/src/main/java/hexlet/code/DifferUtil.java
@@ -1,4 +1,25 @@
 package hexlet.code;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
 
 public class DifferUtil {
+    public static List<Differ> buildDiff(Map<String, Object> oldMap, Map<String, Object> newMap) {
+        TreeSet<String> allKeys = new TreeSet<>();
+
+        allKeys.addAll(oldMap.keySet());
+        allKeys.addAll(newMap.keySet());
+
+        ArrayList<Differ> differ = new ArrayList<>();
+
+        for (String key: allKeys) {
+            Object oldValue = oldMap.get(key);
+            Object newValue = newMap.get(key);
+
+            differ.add(new Differ(key, newValue, oldValue));
+
+        }
+        return differ;
+    }
 }
