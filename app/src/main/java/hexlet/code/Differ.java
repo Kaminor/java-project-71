1162a706cba5b99c450f2099595e067e245f6064
Ivan Zubarev <swim_zb@mail.ru>
2025-06-15 21:51:10 +0700

    add formatter and fix Differ class

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,10 +1,5 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import hexlet.code.generators.JsonGenerator;
-import hexlet.code.generators.PlainGenerator;
-import hexlet.code.generators.StylishGenerator;
 
 public class Differ {
     public static String generate(String filepath1, String filepath2, String formatName) throws Exception {
@@ -13,20 +8,7 @@ public class Differ {
 
         var diffs = DifferUtil.buildDiff(data1, data2);
 
-        if (formatName.equals("json")) {
-            var result = JsonGenerator.generateJson(diffs);
-
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.enable(SerializationFeature.INDENT_OUTPUT);
-
-            return mapper.writeValueAsString(result);
-        }
-
-        if (formatName.equals("plain")) {
-            return PlainGenerator.generatePlain(diffs);
-        }
-
-        return StylishGenerator.generateStylish(diffs);
+        return Formatter.formatGenerate(diffs, formatName);
     }
 
     public static String generate(String filepath1, String filepath2) throws Exception {
--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,4 +1,27 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import hexlet.code.generators.JsonGenerator;
+import hexlet.code.generators.PlainGenerator;
+import hexlet.code.generators.StylishGenerator;
+import java.util.List;
+
 public class Formatter {
+    public static String formatGenerate(List<DifferData> diffs, String formatName) throws Exception {
+        if (formatName.equals("json")) {
+            var result = JsonGenerator.generateJson(diffs);
+
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+            return mapper.writeValueAsString(result);
+        }
+
+        if (formatName.equals("plain")) {
+            return PlainGenerator.generatePlain(diffs);
+        }
+
+        return StylishGenerator.generateStylish(diffs);
+    }
 }
