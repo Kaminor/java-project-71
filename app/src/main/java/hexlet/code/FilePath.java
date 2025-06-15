ef29d294a178b094ddbfa1724e19348b3c5a34f5
Ivan Zubarev <swim_zb@mail.ru>
2025-06-15 21:52:24 +0700

    move getting paths to separate class

--- app/src/main/java/hexlet/code/FilePath.java
+++ app/src/main/java/hexlet/code/FilePath.java
@@ -1,4 +1,16 @@
 package hexlet.code;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FilePath {
+    public static Path getFixturePath(String fileName) {
+        return Paths.get("src", "test", "resources", "fixtures", fileName);
+    }
+
+    public static String readFixture(String fileName) throws Exception {
+        var path = getFixturePath(fileName);
+
+        return Files.readString(path);
+    }
 }
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,7 +1,4 @@
 package hexlet.code;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 import java.util.stream.Collectors;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -9,23 +6,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 public class Parser {
-    public static Path getFixturePath(String fileName) {
-        Path path = Paths.get(fileName);
 
-        if (!path.isAbsolute()) {
-            path = Paths.get("src", "test", "resources", "fixtures", fileName);
-        }
-        return path.toAbsolutePath().normalize();
-    }
-
-    public static String readFixture(String fileName) throws Exception {
-        var path = getFixturePath(fileName);
-        return Files.readString(path);
-    }
 
     public static Map<String, Object> parse(String fileName) throws Exception {
         ObjectMapper objMapper = new ObjectMapper(new YAMLFactory());
-        var readFile = readFixture(fileName);
+        var readFile = FilePath.readFixture(fileName);
 
         Map<Object, Object> dataMap = objMapper.readValue(readFile, new TypeReference<Map<Object, Object>>() { });
 
