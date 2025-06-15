312442cc1852755a360f6251e578c38fb9b7d50e
Ivan Zubarev <swim_zb@mail.ru>
2025-06-15 23:14:20 +0700

    fix paths

--- app/src/main/java/hexlet/code/FilePath.java
+++ app/src/main/java/hexlet/code/FilePath.java
@@ -4,8 +4,16 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FilePath {
-    public static Path getFixturePath(String fileName) {
-        return Paths.get("src", "test", "resources", "fixtures", fileName);
+    public static Path getFixturePath(String fileName) throws Exception {
+        String userDir = System.getProperty("user.dir");
+        Path path;
+
+        if (userDir.contains("project")) {
+            path = Paths.get(userDir, "src", "test", "resources", "fixtures", fileName);
+        } else {
+            path = Paths.get("src", "test", "resources", "fixtures", fileName);
+        }
+        return path.toAbsolutePath().normalize();
     }
 
     public static String readFixture(String fileName) throws Exception {
--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -43,4 +43,17 @@ public class DifferTest {
         var path = getFixturePath(fileName);
         return Files.readString(path);
     }
+
+    public static String generate(String filepath1, String filepath2, String formatName) throws Exception {
+        var data1 = Parser.parse(filepath1);
+        var data2 = Parser.parse(filepath2);
+
+        var diffs = DifferUtil.buildDiff(data1, data2);
+
+        return Formatter.formatGenerate(diffs, formatName);
+    }
+
+    public static String generate(String filepath1, String filepath2) throws Exception {
+        return generate(filepath1, filepath2, "stylish");
+    }
 }
