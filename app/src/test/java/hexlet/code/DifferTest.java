9686ec98f442e7251fa40d2fd7f16dd806b5dbf0
Ivan Zubarev <swim_zb@mail.ru>
2025-06-19 21:24:03 +0700

    change getting paths in tests

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -3,10 +3,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public final class DifferTest {
@@ -27,42 +23,14 @@ public final class DifferTest {
     @ParameterizedTest
     @MethodSource("provideTestCases")
     void testGenerate(String file1, String file2, String format, String expectedFile) throws Exception {
-        String expected = readFixture(expectedFile);
-        var content1 = readFixture(file1);
-        var content2 = readFixture(file2);
+        var expectedPath = DifferTest.class.getClassLoader().getResource("fixtures/" + expectedFile).getPath();
+        var expectedContent = FilePath.readFixture(expectedPath);
+        var resourceUrl1 = DifferTest.class.getClassLoader().getResource("fixtures/" + file1).getPath();
+        var resourceUrl2 = DifferTest.class.getClassLoader().getResource("fixtures/" + file2).getPath();
         String actual = format == null
-                                ? generate(content1, content2)
-                                : generate(content1, content2, format);
+                                ? Differ.generate(resourceUrl1, resourceUrl2)
+                                : Differ.generate(resourceUrl1, resourceUrl2, format);
 
-        assertEquals(expected, actual);
-    }
-
-
-    public static Path getFixturePath(String fileName) throws Exception {
-        var resourceUrl = DifferTest.class.getClassLoader().getResource("fixtures/" + fileName);
-
-        if (resourceUrl == null) {
-            throw new IllegalArgumentException("Not found: " + resourceUrl);
-        }
-
-        return Paths.get(resourceUrl.toURI());
-    }
-
-    public static String readFixture(String fileName) throws Exception {
-        var path = getFixturePath(fileName);
-        return Files.readString(path);
-    }
-
-    public static String generate(String filepath1, String filepath2, String formatName) throws Exception {
-        var data1 = Parser.parse(filepath1);
-        var data2 = Parser.parse(filepath2);
-
-        var diffs = DifferUtil.buildDiff(data1, data2);
-
-        return Formatter.formatGenerate(diffs, formatName);
-    }
-
-    public static String generate(String filepath1, String filepath2) throws Exception {
-        return generate(filepath1, filepath2, "stylish");
+        assertEquals(expectedContent, actual);
     }
 }
