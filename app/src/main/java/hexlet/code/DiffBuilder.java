4fe0b43e29a9ffe961f8bf4fd511c4b462df2c3d
Ivan Zubarev <swim_zb@mail.ru>
2025-12-01 19:44:28 +0300

    fix: исправление логики в DifferTest

--- app/src/main/java/hexlet/code/DiffBuilder.java
+++ app/src/main/java/hexlet/code/DiffBuilder.java
@@ -1,9 +1,5 @@
 package hexlet.code;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class DiffBuilder {
     public static List<DifferData> buildDiff(Map<String, Object> oldMap, Map<String, Object> newMap) {
@@ -15,10 +11,23 @@ public class DiffBuilder {
         List<DifferData> diff = new ArrayList<>();
 
         for (String key: allKeys) {
+            boolean oldHas = oldMap.containsKey(key);
+            boolean newHas = newMap.containsKey(key);
             Object oldValue = oldMap.get(key);
             Object newValue = newMap.get(key);
 
-            if (oldValue != null && newValue == null) {
+            //TODO: доделать логику с существованием ключей в мапе (статусы added и updated)
+            if (oldHas && !newHas) {
+                diff.add(new DifferData("removed", key, null, oldValue));
+            } else if (!oldHas && newHas) {
+                diff.add(new DifferData("added", key, newValue, null));
+            } else if (Objects.equals(oldValue, newValue)) {
+                diff.add(new DifferData("unchanged", key, newValue, null));
+            } else if (!Objects.equals(oldValue, newValue)) {
+                diff.add(new DifferData("updated", key, newValue, oldValue));
+            }
+
+            /*if (oldValue != null && newValue == null) {
                 diff.add(new DifferData("removed", key, null, oldValue));
             } else if (oldValue == null && newValue != null) {
                 diff.add(new DifferData("added", key, newValue, null));
@@ -26,7 +35,7 @@ public class DiffBuilder {
                 diff.add(new DifferData("updated", key, newValue, oldValue));
             } else if (oldValue != null && newValue != null && oldValue.equals(newValue)) {
                 diff.add(new DifferData("unchanged", key, newValue, null));
-            }
+            }*/
 
         }
         return diff;
